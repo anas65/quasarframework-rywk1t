0c9e45b43f04009c06f5e0ddad18a1178611f82e
anas65 <mhamedya@gmail.com>
2023-02-24 14:42:55 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quasarframework-rywk1t
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quasarframework-rywk1t)
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row fullscreen bg-grey-3">
+    <div class="column bg-grey-10 q-gutter-y-sm items-center q-py-sm" style="width:80px;">
+      <q-btn color="grey-8" icon="home"></q-btn>
+      <q-btn color="grey-8" icon="event"></q-btn>
+    </div>
+    <div class="col">f</div>
+  </div>
+</template>
+
+<script setup></script>
